71773d700b84435d2e0fd47b46c0778cc3146fc8
Beau Kim <beaukim3@gmail.com>
2023-03-13 23:59:37 -0400

    complete

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
+    def talk(self):
+        print("Hello World!")
 
+    def walk(self):
+        print("The person is walking.")
+
+Ben = Person()
+Ben.talk()
+Ben.walk()
     #Instance method definition
-    pass
